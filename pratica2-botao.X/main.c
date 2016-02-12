5ab39e8fe0bf1aed4e143fae1c3176a0a7bc9209
Rodrigues Filho <ednofco@gmail.com>
2016-02-12 08:50:47 -0300

    commit inicial

---
+++ pratica2-botao.X/main.c
@@ -0,0 +1,22 @@
+/*
+ * File:   main.c
+ * Author: edno
+ *
+ * Created on 12 de Fevereiro de 2016, 09:47
+ */
+
+
+#include <xc.h>
+#include <p18f2550.h>
+
+#include "configuration.h"
+
+#define LED LATB1
+#define BTN RB0
+
+void main(void) {
+
+    TRISBbits_t.TRISB1 = 0;
+    
+    return;
+}
---
+++ pratica2-botao.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=pratica2-botao.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/pratica2-botao.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=pratica2-botao.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/pratica2-botao.x.tar
