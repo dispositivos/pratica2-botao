e329b45c28fd1b8f64a79e5146f81098c7595197
Rodrigues Filho <ednofco@gmail.com>
2016-02-12 09:39:00 -0300

    Concluido parte inicial do projeto com botao, sem interrupcao

--- pratica2-botao.X/main.c
+++ pratica2-botao.X/main.c
@@ -8,15 +8,26 @@
 
 #include <xc.h>
 #include <p18f2550.h>
+#include "configuration.h" // Inclui as configurações do pic
 
-#include "configuration.h"
+#define _XTAL_FREQ 20e+6
 
-#define LED LATB1
-#define BTN RB0
+#define BTN RB5 // Define pino 5 como entrada para botao
+#define LED LATB1 // LED é uma saída
 
 void main(void) {
 
-    TRISBbits_t.TRISB1 = 0;
+    PORTB = 0x00; // inicializa a porta B com saída em zero
+    TRISBbits.TRISB1 = 0; // define LED como saída
+
+    // loop infinito
+    while(1){
+        if(BTN == 0) { // se o botao for pressionado
+            LED = 1; // liga o led
+            while(!BTN); // aguarda botao se liberado
+            LED = 0; // desliga o led
+        }
+    }
     
     return;
 }
